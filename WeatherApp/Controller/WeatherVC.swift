b2fa1914c5f95be92fa15e658f3f1e6be2281561
Rick <rmartinez1611@gmail.com>
2020-11-19 14:11:21 -0500

    Connected VC and Cell to Classes

---
+++ WeatherApp/Controller/WeatherVC.swift
@@ -0,0 +1,35 @@
+//
+//  WeatherVC.swift
+//  WeatherApp
+//
+//  Created by Ricardo Martinez on 11/19/20.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    @IBOutlet weak var todaysDateLabel:       UILabel!
+    @IBOutlet weak var locationLabel:         UILabel!
+    @IBOutlet weak var weatherLabel:          UILabel!
+    @IBOutlet weak var imageView:             UIImageView!
+    @IBOutlet weak var todayAndWeeklyControl: UISegmentedControl!
+    @IBOutlet weak var collectionVIew:        UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WeatherApp/View/WeatherCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by Ricardo Martinez on 11/19/20.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var topLabel:    UILabel!
+    @IBOutlet weak var imageView:   UIImageView!
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+}
